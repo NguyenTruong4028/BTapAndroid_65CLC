9c8ce5f9345ba8081e3b21b977681723f9df7e0f
Nguyen Truong <truong.nh.4028@gmail.com>
2025-10-22 09:49:33 +0700

    change secondactivity

--- ViDuIntent/app/src/main/java/ntu/nguyentruong/viduintent/MainActivity.java
+++ ViDuIntent/app/src/main/java/ntu/nguyentruong/viduintent/MainActivity.java
@@ -1,6 +1,11 @@
 package ntu.nguyentruong.viduintent;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +14,29 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnSend;
+    EditText etName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSend = findViewById(R.id.btnSend);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Code chuyen man hinh
+                //1. Tao intent
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                //2. Goi du lieu vao intent
+                //2.1 Lay du lieu
+                etName = findViewById(R.id.etSend);
+                String data= etName.getText().toString();
+                //2.2 Goi vao intent, dung put..Extra(key,value)
+                intent.putExtra("HT",data);
+                intent.putExtra("Copyright","NHT");
+                //3. Chuyen man hinh
+                startActivity(intent);
+            }
+        });
     }
 }
--- ViDuIntent/app/src/main/java/ntu/nguyentruong/viduintent/SecondActivity.java
+++ ViDuIntent/app/src/main/java/ntu/nguyentruong/viduintent/SecondActivity.java
@@ -1,6 +1,10 @@
 package ntu.nguyentruong.viduintent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +13,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SecondActivity extends AppCompatActivity {
-
+    TextView tvNhan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
+        Intent iNhanDuoc = getIntent();
+        String htNhanDuoc = iNhanDuoc.getStringExtra("HT");
+        tvNhan = findViewById(R.id.tvBack);
+        tvNhan.setText(htNhanDuoc);
+        Button btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iBack = new Intent(SecondActivity.this,MainActivity.class);
+                startActivity(iBack);
+            }
+        });
     }
 }
