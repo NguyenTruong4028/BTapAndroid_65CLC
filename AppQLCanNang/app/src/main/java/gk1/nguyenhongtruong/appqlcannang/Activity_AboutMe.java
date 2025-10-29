4a8bc533ea55d9fbc4e9dbb05d6c313cbe73722a
Nguyen Truong <truong.nh.4028@gmail.com>
2025-10-29 07:48:13 +0700

    update

---
+++ AppQLCanNang/app/src/main/java/gk1/nguyenhongtruong/appqlcannang/Activity_AboutMe.java
@@ -0,0 +1,18 @@
+package gk1.nguyenhongtruong.appqlcannang;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Activity_AboutMe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_me);
+    }
+}
---
+++ AppQLCanNang/app/src/main/java/gk1/nguyenhongtruong/appqlcannang/Activity_BaiThuoc.java
@@ -0,0 +1,18 @@
+package gk1.nguyenhongtruong.appqlcannang;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Activity_BaiThuoc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai_thuoc);
+    }
+}
