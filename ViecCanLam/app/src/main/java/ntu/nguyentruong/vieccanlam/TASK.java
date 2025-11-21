02cd831c9a2587fc3b8a99de4a928d7e5fb3397c
Nguyen Truong <truong.nh.4028@gmail.com>
2025-11-21 11:22:32 +0700

    Viec can lam + firebase

---
+++ ViecCanLam/app/src/main/java/ntu/nguyentruong/vieccanlam/TASK.java
@@ -0,0 +1,47 @@
+package ntu.nguyentruong.vieccanlam;
+
+public class TASK {
+    String name, message, date, priority;
+
+    public TASK() {
+    }
+
+    public TASK(String name, String message, String date, String priority) {
+        this.name = name;
+        this.message = message;
+        this.date = date;
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
